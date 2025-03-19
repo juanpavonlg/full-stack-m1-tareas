dd6228057307dca76b1fd898d0caad5e7ea837cb
juanpavon <juanpavonlg@gmail.com>
2025-03-19 17:12:43 -0400

    se agrego la funcionalidad para buscar tareas por estado, criterio o fecha

--- 07-tasks/backend/controllers/taskController.js
+++ 07-tasks/backend/controllers/taskController.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { Task } = require("../models");
+const { Op } = require("sequelize");
 
 exports.createTask = async (req, res) => {
   const { title, description, status, dueDate } = req.body;
@@ -21,6 +22,37 @@ exports.createTask = async (req, res) => {
 
 exports.getTasks = async (req, res) => {
   const userId = req.user.id;
+  const status = req.query.status;
+  const search = req.query.search;
+  const dueDate = req.query.dueDate;
+  if (status) {
+    const tasks = await Task.findAll({ where: { userId, status } });
+    return res.status(200).json(tasks);
+  } else if (search) {
+    const tasks = await Task.findAll({
+      where: {
+        userId,
+        [Op.or]: [
+          {
+            title: {
+              [Op.iLike]: `%${search}%`,
+            },
+          },
+          {
+            description: {
+              [Op.iLike]: `%${search}%`,
+            },
+          },
+        ],
+      },
+    });
+    return res.status(200).json(tasks);
+  } else if (dueDate) {
+    const tasks = await Task.findAll({
+      where: { userId, dueDate: { [Op.lte]: `%${dueDate}%` } },
+    });
+    return res.status(200).json(tasks);
+  }
   const tasks = await Task.findAll({ where: { userId } });
   return res.status(200).json(tasks);
 };
@@ -45,12 +77,18 @@ exports.updateTask = async (req, res) => {
   const statuses = { pendiente: "en progreso", "en progreso": "completada" };
   const status = req.body.status;
   if (task.status === "completada") {
-    return res.status(400).json({ message: "No se puede modificar una tarea 'completada'" });
+    return res
+      .status(400)
+      .json({ message: "No se puede modificar una tarea 'completada'" });
   } else if (status !== task.status && status !== statuses[task.status]) {
     if (task.status === "pendiente") {
-      return res.status(400).json({ message: "Una tarea 'pendiente' sólo puede pasar a 'en progreso'" });
+      return res.status(400).json({
+        message: "Una tarea 'pendiente' sólo puede pasar a 'en progreso'",
+      });
     } else if (task.status === "en progreso") {
-      return res.status(400).json({ message: "Una tarea 'en progreso' sólo puede pasar a 'completada'" });
+      return res.status(400).json({
+        message: "Una tarea 'en progreso' sólo puede pasar a 'completada'",
+      });
     }
   }
   await task.update(req.body);
@@ -64,6 +102,6 @@ exports.deleteTask = async (req, res) => {
   if (!task) {
     return res.status(404).json({ message: "Tarea no encontrada" });
   }
-  await task.destroy()
+  await task.destroy();
   return res.status(200).json({ message: "Tarea eliminada con éxito", task });
 };
